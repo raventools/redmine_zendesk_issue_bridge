80c4ad38f2b38d8f3062765ed1bbe839c3e7050b
LEE_SEUNGHUN06 <lee_seunghun06@elandinnople.com>
2023-07-14 09:14:18 +0900

    etc: zendesk custom ticket update and ticket status managemnt

--- app/controllers/redmine_zendesk_issue_bridge_controller.rb
+++ app/controllers/redmine_zendesk_issue_bridge_controller.rb
@@ -3,20 +3,60 @@ class RedmineZendeskIssueBridgeController < ApplicationController
   unloadable
 
   layout false
-  before_filter :require_login
+  before_action :require_login
 
   def index
     @zendesk_subdomain = Issue.zendesk_settings[:zendesk_subdomain]
     @related_zendesk_tickets = related_zendesk_tickets()
   end
 
+  def update_ticket_status
+    ticket_id = params[:ticket_id]
+    ticket = zendesk_client.tickets.find(id: ticket_id)
+    if ticket
+      status = params[:status]
+
+      if status == 'hold'
+        ticket.status = 'hold'
+      elsif status == 'open'
+        ticket.status = 'open'
+      else
+        render json: { success: false, message: 'Invalid status value.'}
+        return
+      end
+      ticket.save
+      render json: { success: true, message: 'Ticket status updated successfully.' }
+    else
+      Rails.logger.info("Ticket status: fail")
+      render json: { success: false, message: 'Ticket not found.' }
+    end
+  end
+
+  def update_ticket_field_value
+      ticket_id = params[:ticket_id]
+      ticket = zendesk_client.tickets.find(id: ticket_id)
+      Rails.logger.info("Ticket found: #{ticket.inspect}")
+      if ticket
+        issueNumber = params[:issueNumber]
+        ticket.custom_fields = [
+          { id: 'custom_ticket_number', value: issueNumber }
+        ]
+        ticket.save
+      end
+  end
+
 private
 	def related_zendesk_tickets
 		tickets = []
 		retrieved = zendesk_client.search query: "fieldvalue:#{params[:id]}"
-		retrieved.each do |ticket|
-			tickets << ticket if matches_on_custom_field?(ticket[:fields])
-		end
+                if retrieved.empty?
+                else
+                  retrieved.each do |ticket|
+                    next if tickets.any? { |t| t[:id] == ticket[:id] }
+                    Rails.logger.info("Adding Ticket ID: #{ticket[:id]}")
+                    tickets << ticket if matches_on_custom_field?(ticket[:fields])
+                  end
+                end
 		tickets
 	rescue
 		[]
@@ -44,10 +84,10 @@ private
 		require 'zendesk_api'
 		ZendeskAPI::Client.new do |config|
 			subdomain = Issue.zendesk_settings[:zendesk_subdomain]
-			config.url = "https://#{subdomain}.zendesk.com/api/v2" # e.g. https://mydesk.zendesk.com/api/v2
-			config.username = Issue.zendesk_settings[:zendesk_username]
-			config.password = Issue.zendesk_settings[:zendesk_password]
-			config.retry = true
+			config.url = "https://elandsystems.zendesk.com/api/v2" # e.g. https://mydesk.zendesk.com/api/v2
+                        config.username = "your zendesk username/token"
+                        config.token = "your zendesk token"
+                        config.retry = true
 		end
 	end
 
--- lib/redmine_zendesk_issue_bridge/retrieve_ticket_data.rb
+++ lib/redmine_zendesk_issue_bridge/retrieve_ticket_data.rb
@@ -1,13 +1,11 @@
-require_dependency 'issue'
+module RedmineZendeskIssueBridge
+  module RetrieveTicketData
+    def self.included(base) # :nodoc:
+      base.extend(ClassMethods)
 
-# Patches Redmine's Issues dynamically.
-module IssuePatch
-  def self.included(base) # :nodoc:
-    base.extend(ClassMethods)
-
-    # Same as typing in the class
-    base.class_eval do
-      unloadable # Send unloadable so it will not be unloaded in development
+      base.class_eval do
+        unloadable # Send unloadable so it will not be unloaded in development
+      end
     end
   end
 
@@ -19,5 +17,29 @@ module IssuePatch
   end
 end
 
-# Add module to Issue
-Issue.send(:include, IssuePatch)
+Issue.send(:include, RedmineZendeskIssueBridge::RetrieveTicketData)
+#
+
+# require_dependency 'issue'
+#
+# # Patches Redmine's Issues dynamically.
+# module IssuePatch
+#   def self.included(base) # :nodoc:
+#     base.extend(ClassMethods)
+#
+#     # Same as typing in the class
+#     base.class_eval do
+#       unloadable # Send unloadable so it will not be unloaded in development
+#     end
+#   end
+#
+#   # Class Methods
+#   module ClassMethods
+#     def zendesk_settings
+#       Setting.plugin_redmine_zendesk_issue_bridge || {}
+#     end
+#   end
+# end
+#
+# # Add module to Issue
+# Issue.send(:include, IssuePatch)
